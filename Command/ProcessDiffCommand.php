77c4797b780b4f5315b2af208831e31018183281
Rick Prent <rprent@hostnet.nl>
2012-10-16 15:42:28 +0200

    Modified the ProcessDiffCommand based on review feedback, Review: #44801.

--- Command/ProcessDiffCommand.php
+++ Command/ProcessDiffCommand.php
@@ -2,6 +2,8 @@
 
 namespace Hostnet\HostnetCodeQualityBundle\Command;
 
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
 use Symfony\Component\Console\Command\Command,
     Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputInterface,
@@ -10,22 +12,39 @@ use Symfony\Component\Console\Command\Command,
 
 use Doctrine\Common\Collection;
 
-class ProcessDiffCommand extends Command
+/**
+ * Processes the diff by calling the cq:processDiff command on the CLI.
+ * Input: php app/console cq:processDiff path_to_diff review_request_id [-register|-r]
+ * Example: php app/console cq:processDiff path/to/diff 43261 -register false
+ *
+ * @author rprent
+ */
+class ProcessDiffCommand extends ContainerAwareCommand
 {
+  /**
+   * Configures the command settings
+   *
+   * @see \Symfony\Component\Console\Command\Command::configure()
+   */
   protected function configure()
   {
     $this
       ->setName('cq:processDiff')
       ->setDescription('Scans the diff on the quality of the code and returns feedback.')
       ->setDefinition(array(
-        new InputArgument('path_to_diff', InputArgument::REQUIRED, 'Path to the diff'),
+        new InputArgument('path_to_diff', InputArgument::REQUIRED, 'Path to the diff / patch file'),
         new InputArgument('review_request_id', InputArgument::REQUIRED,
           'The id of the review request to give feedback on'),
-        new InputArgument('register', InputArgument::OPTIONAL, 'Register the diff')
+        new InputOption('register', 'r', InputOption::VALUE_REQUIRED, 'Register the diff', false)
       ))
     ;
   }
 
+  /**
+   * Executes the command
+   *
+   * @see \Symfony\Component\Console\Command\Command::execute()
+   */
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     // User CLI Input
@@ -34,20 +53,16 @@ class ProcessDiffCommand extends Command
     $diff = file_get_contents('/' . $path_to_diff);
     // Review request id arg
     $review_request_id = $input->getArgument('review_request_id');
-    // If the optional register arg is set use it, otherwise default to false
-    $register = isset($input->getArgument('register')) ?
-      $input->getArgument('register') : false;
+    // If the optional register arg is set put it on true, otherwise default to false
+    $register = $input->getOption('register');
+    if(isset($register) && $register !== false) {
+      $register = true;
+    }
 
-    // Get the container from the kernel in order to call all the required services.
-    $this->container = $this->getApplication()->getKernel()->getContainer();
-    // All the required services
-    $em = $this->container->get('doctrine')->getEntityManager();
-    $tools = $this->container->get('entity_factory')->retrieveTools();
+    // Process the review by calling the ReviewProcessor through the container
     $review = $this->container->get('review_processor')->processReview(
       $diff,
-      $register,
-      $em,
-      $tools
+      $register
     );
   }
 }

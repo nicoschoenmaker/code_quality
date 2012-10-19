d87e8485111a234c54426a2abdbf8f8361a7f1e4
Rick Prent <rprent@hostnet.nl>
2012-10-19 09:45:34 +0200

    New modifications based on review feedback input. Review: #44801.

--- Command/ProcessLocalDiffCommand.php
+++ Command/ProcessLocalDiffCommand.php
@@ -6,18 +6,19 @@ use Symfony\Component\Console\Command\Command,
     Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputInterface,
     Symfony\Component\Console\Input\InputOption,
-    Symfony\Component\Console\Output\OutputInterface;
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 use Doctrine\Common\Collection;
 
 /**
- * Processes the diff by calling the cq:processDiff command on the CLI.
- * Input: php app/console cq:processDiff path_to_diff review_request_id [-register|-r]
- * Example: php app/console cq:processDiff path/to/diff 43261 -register false
+ * Processes the local supplied diff by calling the cq:processDiff:localDiff command on the CLI.
+ * Input:   php app/console cq:processDiff:localDiff path_to_diff [--register|-r]
+ * Example: php app/console cq:processDiff:localDiff path/to/diff     -r true
  *
  * @author rprent
  */
-class ProcessDiffCommand extends Command
+class ProcessLocalDiffCommand extends ContainerAwareCommand
 {
   /**
    * Configures the command settings
@@ -27,13 +28,11 @@ class ProcessDiffCommand extends Command
   protected function configure()
   {
     $this
-      ->setName('cq:processDiff')
+      ->setName('cq:processDiff:localDiff')
       ->setDescription('Scans the diff on the quality of the code and returns feedback.')
       ->setDefinition(array(
-        new InputArgument('path_to_diff', InputArgument::REQUIRED, 'Path to the diff / patch file'),
-        new InputArgument('review_request_id', InputArgument::REQUIRED,
-          'The id of the review request to give feedback on'),
-        new InputOption('register', 'r', InputOption::VALUE_REQUIRED, 'Register the diff', false)
+        new InputArgument('path_to_diff', InputArgument::REQUIRED, 'Path to the diff / patch file.'),
+        new InputOption('register', 'r', InputOption::VALUE_REQUIRED, 'Register the diff.', false)
       ))
     ;
   }
@@ -49,17 +48,14 @@ class ProcessDiffCommand extends Command
     // Path to diff arg & retrieve the diff
     $path_to_diff = $input->getArgument('path_to_diff');
     $diff = file_get_contents('/' . $path_to_diff);
-    // Review request id arg
-    $review_request_id = $input->getArgument('review_request_id');
     // If the optional register arg is set put it on true, otherwise default to false
     $register = $input->getOption('register');
     if(isset($register) && $register !== false) {
       $register = true;
     }
 
-    $this->container = $this->getApplication()->getKernel()->getContainer();
     // Process the review by calling the ReviewProcessor through the container
-    $review = $this->container->get('review_processor')->processReview(
+    $review = $this->getContainer()->get('review_processor')->processReview(
       $diff,
       $register
     );
